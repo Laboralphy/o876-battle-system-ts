b4c6b273443e5b8540ae353d31ca7929333723e4
ralphy <raphael.marandet@gmail.com>
2026-01-28 02:47:19 +0100

    Dice class

---
+++ src/schemas/enums/race.ts
@@ -0,0 +1,14 @@
+import z from 'zod';
+import CONSTS from '../consts';
+
+export const RaceSchema = z
+    .enum([
+        CONSTS.RACE_HUMAN,
+        CONSTS.RACE_ELF,
+        CONSTS.RACE_DWARF,
+        CONSTS.RACE_HALFLING,
+        CONSTS.RACE_UNKNOWN,
+    ])
+    .describe('fields.race');
+
+export type Race = z.infer<typeof RaceSchema>;
